15d5f70e17ab121a65d5fd4bb701630ed0f3a31d
HgaerRabea <hagerrabea12@gmail.com>
2021-09-13 00:04:00 +0200

    keyboard

--- js/main.js
+++ js/main.js
@@ -69,3 +69,41 @@ smallBox.addEventListener('click', function (e) {
     e.stopPropagation();
 
 })
+
+// document.onkeyup = function (e) {
+//     e = e || window.event;
+    
+//     // 27 is the ESC key
+//     if(e.keyCode === 27) modal.style.display = "none";
+//     };
+
+    document.addEventListener("keyup",function(e){
+        if(e.keyCode == 27){
+         fixedBox.classList.replace("d-flex","d-none");
+        }
+     });
+
+     document.addEventListener("keyup",function(e){
+        if(e.keyCode == 39){
+                index++;
+                if(index==imgArray.length){
+                    index=0;
+                }
+                var x=imgArray[index];
+                var imgSrc=x.getAttribute("src");
+                smallBox.style.backgroundImage= `url(${imgSrc})`;
+            
+        }
+     });
+     document.addEventListener("keyup",function(e){
+        if(e.keyCode == 37){
+            index--;
+            if(index<0){
+                index=imgArray.length-1;
+            }
+            var x=imgArray[index];
+            var imgSrc=x.getAttribute("src");
+            smallBox.style.backgroundImage= `url(${imgSrc})`;
+            
+        }
+     });
